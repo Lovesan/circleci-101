74a98c77c1765c2d3038c8d7c9ccf17677ca02d8
Dmitry Ignatiev <lovesan.ru@gmail.com>
2019-05-23 21:01:41 +0300

    Nightly workflow

--- test/test.lisp
+++ test/test.lisp
@@ -6,6 +6,9 @@
 
 (in-package #:circleci-101-test)
 
+(defvar *run-integration-tests* (let ((env (uiop:getenv "RUN_INTEGRATION_TESTS")))
+                                  (and env (string-equal env "t"))))
+
 (def-suite circleci-101-suite
   :description "Tests")
 
@@ -13,3 +16,6 @@
 
 (test test-hello
       (is (eq t t)))
+
+(when *run-integration-tests*
+    (test test-integration (is (eq t t))))
